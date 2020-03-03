d4a801d90194d8539fe7e5ca9aa6337f83a71d38
Evgeniy Uskov <driftrboy@gmail.com>
2020-03-03 18:43:56 +0700

    Initial Commit

---
+++ InterviewTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InterviewTestApp
+//
+//  Created by Evgeniy Uskov on 03.03.2020.
+//  Copyright © 2020 Evgeniy Uskov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
